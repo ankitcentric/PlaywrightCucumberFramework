55485e7f077b0f4cd62c00ff19ba2e6effa9cb0a
ankitcentric <ankit.sharma2@centricconsulting.com>
2025-05-30 14:04:05 +0530

    Creating branch rule

--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -6,7 +6,7 @@ Then(/^the "([^"]*)" should contains the text "(.*)"$/, async function(elementKe
         screen: {page},
     }=this;
     const result = await page.textContent("span[id='Automation_Practice'] span strong span span");
-    console.log(`the ${elementKey} should contain the text- ${expectedText}`);
+    console.log(` the ${elementKey} should contain the text- ${expectedText}`);
     expect(result).toBe(expectedText);
 });
 
@@ -14,7 +14,7 @@ Then(/^the "([^"]*)" should be displayed$/, async function(elementKey: string) {
      const {
         screen: {page},
     }=this;
-    console.log(`the ${elementKey} should Displayed`);
+    console.log(` the ${elementKey} should Displayed`);
     const locator = page.locator("div.et_pb_menu__logo img")
     await expect(locator).toBeVisible();
 });
